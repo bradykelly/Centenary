e215839cc188298f655e908e7b721a295cf4ce93
bradykelly <brady@bradykelly.net>
2022-03-05 03:35:18 +0200

    Empty and FrontEgg react apps

--- Centenary.Mvc/Program.cs
+++ Centenary.Mvc/Program.cs
@@ -1,7 +1,20 @@
+using Centenary.Mvc;
+using Frontegg.SDK.AspNet;
+using Frontegg.SDK.AspNet.Proxy;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.VisualBasic;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.TryAddSingleton<IFronteggProxyInfoExtractor, MyFronteggProxyInfoExtractor>();
+builder.Services.AddFrontegg(options =>
+{
+    options.ApiKey = builder.Configuration["Frontegg:ApiKey"];
+    options.ClientId = builder.Configuration["Frontegg:ClientId"];
+    options.ThrowOnMissingConfiguration = true;
+});
 
 var app = builder.Build();
 
@@ -12,13 +25,17 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
+app.UseFrontegg<MyFronteggProxyInfoExtractor>();
 
 app.MapControllerRoute(
     name: "default",
