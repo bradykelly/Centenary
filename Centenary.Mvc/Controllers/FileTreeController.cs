34efc600b8adbef15a2a10b990e37492e2d9f6b4
bradykelly <brady@bradykelly.net>
2022-03-12 06:20:55 +0200

    New MVC structure with FileTreeController

---
+++ Centenary.Mvc/Controllers/FileTreeController.cs
@@ -0,0 +1,22 @@
+﻿using Centenary.Storage;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Centenary.Mvc.Controllers;
+
+public class FileTreeController : Controller
+{
+    private readonly IBlobApiClient _blobApiClient;
+    private readonly ILogger<FileTreeController> _logger;
+
+    public FileTreeController(IBlobApiClient blobApiClient, ILogger<FileTreeController> logger, ILogger<FileTreeController> logger1)
+    {
+        _blobApiClient = blobApiClient;
+        _logger = logger1;
+    }
+    
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
