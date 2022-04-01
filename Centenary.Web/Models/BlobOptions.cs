d9c3d3facd6a911051d6bb3c368c11d7e9b90ff3
Brady Kelly <brady.kelly@xisystems.co.za>
2022-04-01 23:19:21 +0200

    Removed React.Web

--- Centenary.Web/Models/BlobOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Centenary.Web.Models;
-
-public class BlobOptions
-{
-    public string PathDelimiter { get; set; } = string.Empty;
-    public string DefaultContainer { get; set; } = "archive";
-    public string EmptyFolderFilename { get; set; } = "__empty__.txt";
-    public string SystemUserName { get; set; } = "System";
-}
